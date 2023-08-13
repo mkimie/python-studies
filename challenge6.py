5b08ebacc6e34633e079b2d00bc02d3442b04865
Monique Kimie <mkhamatsu@outlook.com>
2023-08-13 14:25:59 -0300

    challenge6: listas

---
+++ challenge6.py
@@ -0,0 +1,12 @@
+'''
+Curso: Programação Python do Zero ao Avançado + 32 Projetos Reais
+Instrutor: André Iacono
+
+Desafio: Para este desafio, queria que você criasse uma lista chamada 'frutas'
+que contenha os seguintes itens: 'maça', 'banana', 'manga' e 'uva.
+Depois disso, quero que imprima a lista na tela
+'''
+
+fruits = ['maça', 'banana', 'manga', 'uva']
+
+print(f"Fruits: {fruits}")
