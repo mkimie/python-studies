d47eeddcc8c1f3e021e3d22e3f3e2e3c3b1f9533
Monique Kimie <mkhamatsu@outlook.com>
2023-08-13 14:31:54 -0300

    challenge7: listas e index

---
+++ challenge7.py
@@ -0,0 +1,14 @@
+'''
+Curso: Programação Python do Zero ao Avançado + 32 Projetos Reais
+Instrutor: André Iacono
+
+Desafio: Para este desafio, querio que você use a lista 'frutas' do desafio anterior.
+Seu desafio é imprimir o primeiro e o último elemento da lista.
+'''
+
+fruits = ['maça', 'banana', 'manga', 'uva']
+first_index = 0
+last_index = len(fruits)-1
+
+print(f"Firts fruit: {fruits[first_index]}")
+print(f"Last fruit: {fruits[last_index]}")
