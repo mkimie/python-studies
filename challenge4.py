07273271f5f2221ec44bdafff62fe6b69d072e15
Monique Kimie <mkhamatsu@outlook.com>
2023-08-13 11:53:26 -0300

    challenge4: input() - cast de string para int

--- challenge4.py
+++ challenge4.py
@@ -7,7 +7,7 @@ usando a função input().Depois disso, o script deve imprimir uma mensagem que 
 'Olá[nome]! Você tem [idade] anos.'
 '''
 
-name = input("What's your name?")
-age = input("How old are you?")
+name = input("What's your name? ")
+age = int(input("How old are you? "))
 
 print(f"Hello {name}! You are {age} years old.")
