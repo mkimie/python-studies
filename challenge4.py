5b0931a951da06bfbfa4b467b2b634c59ca6c062
Monique Kimie <mkhamatsu@outlook.com>
2023-08-13 11:45:35 -0300

    challenge4: input()

---
+++ challenge4.py
@@ -0,0 +1,13 @@
+'''
+Curso: Programação Python do Zero ao Avançado + 32 Projetos Reais
+Instrutor: André Iacono
+
+Desafio: Neste desafio, quero que você crie um script que pergunte o nome e a idade do usuário, 
+usando a função input().Depois disso, o script deve imprimir uma mensagem que diga 
+'Olá[nome]! Você tem [idade] anos.'
+'''
+
+name = input("What's your name?")
+age = input("How old are you?")
+
+print(f"Hello {name}! You are {age} years old.")
