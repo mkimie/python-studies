428ee9e3ac65539bfd9ed63a61705031dc34da67
Monique Kimie <mkhamatsu@outlook.com>
2023-08-13 16:08:29 -0300

    challenge11:loops com range()

---
+++ challenge11.py
@@ -0,0 +1,10 @@
+'''
+Curso: Programação Python do Zero ao Avançado + 32 Projetos Reais
+Instrutor: André Iacono
+
+Desafio: Para este desafio, quero que você use a função range() em um for loop'
+para imprimir os números de 1 a 10 na tela.
+'''
+
+for num in range(1,11):
+    print(num)
