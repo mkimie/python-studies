5d819f7ec633733ba21202ec1ad16263943dd58c
Monique Kimie <mkhamatsu@outlook.com>
2023-08-13 11:29:14 -0300

    challenge1: print()

--- challenge_1.py
+++
@@ -1,9 +0,0 @@
-'''
-Curso: Programação Python do Zero ao Avançado + 32 Projetos Reais
-Instrutor: André Iacono
-
-Desafio: Para este desafio, quero que você crie um script que imprima afrase "Olá mundo Python!" no console.
-
-'''
-
-print('Hello, Word Python!')
