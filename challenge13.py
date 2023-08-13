e662413a463c92e7242736b9771fe133d8c58c06
Monique Kimie <mkhamatsu@outlook.com>
2023-08-13 17:42:48 -0300

    challenge13: while loop

---
+++ challenge13.py
@@ -0,0 +1,15 @@
+'''
+Curso: Programação Python do Zero ao Avançado + 32 Projetos Reais
+Instrutor: André Iacono
+
+Desafio: Para este desafio, quero que você use um loop(não pode ser o for loop)
+para imprimir os números de 1 a 10 na tela.
+o vegetal em segundo.
+'''
+
+x= 1
+
+while(x<=10):
+    print(x)
+    x += 1
+
