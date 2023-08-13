386a98524dfe89f4eede06f9a6bd36c1047983a7
Monique Kimie <mkhamatsu@outlook.com>
2023-08-13 11:09:59 -0300

    challenge1: variáveis- ajustes de digitação do desafio

--- challenge_2.py
+++ challenge_2.py
@@ -3,7 +3,7 @@ Curso: Programação Python do Zero ao Avançado + 32 Projetos Reais
 Instrutor: André Iacono
 
 Desafio: Para este desafio, quero que você declare duas variáveis: 
-uma chamada 'nome' que deve guaradar o seu primeiro nome como uma string, 
+uma chamada 'nome' que deve guardar o seu primeiro nome como uma string, 
 e outra chamada 'idade', que deve guardar sua idade como um número inteiro.
 Depois disso, eu quero que você imprima na tela uma frase que diga:
 "Olá, meu nome é [nome] e eu tenho [idade] anos."
