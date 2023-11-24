8cbebd72d22fd248c9438dd2ac39a8b611137e6e
Monique Kimie <mkhamatsu@outlook.com>
2023-11-24 20:27:43 -0300

    challenge16: verificador de números com IF ELSE

---
+++ challenge16.py
@@ -0,0 +1,21 @@
+'''
+Curso: Programação Python do Zero ao Avançado + 32 Projetos Reais
+Instrutor: André Iacono
+
+Desafio: Para este desafio, quero que você peça ao usuário que digite um número.
+Se o número for maior que 10, imprima "O número é maior que 10".
+Caso contrário, imprima "O número é menor ou igual a 10"
+'''
+
+number = int(input("Enter the number: "))
+
+if number > 10 :
+    print("The number is greater than 10.")
+else: 
+    print("The number is less than or equal to 10.")
+
+
+   
+
+
+   
