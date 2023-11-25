33a42b134b0f47a41efe92db01a739d46a4a5940
Monique Kimie <mkhamatsu@outlook.com>
2023-11-25 13:30:35 -0300

    challenge19: encontrando um item com while

---
+++ challenge19.py
@@ -0,0 +1,24 @@
+'''
+Curso: Programação Python do Zero ao Avançado + 32 Projetos Reais
+Instrutor: André Iacono
+
+Desafio: Para este desafio, crie um loop que peça ao usuário para digitar o nome de uma fruta.
+Se a fruta digitada não for 'abacate', o loop deve continuar pedindo ao usuário para digitar o nome de uma fruta.
+Se a fruta for 'abacate', o loop deve terminar e o programa deve imprimir "Parabéns, você acertou a fruta!"
+'''
+
+
+fruit = ''
+while fruit != 'abacate':
+    fruit = input("Enter a fruit: ")
+    continue
+if fruit == 'abacate':
+    print("Congratulations, you got the fruit right!")
+
+
+
+
+   
+
+
+   
